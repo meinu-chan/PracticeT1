ce3f4dcb0bfa69d4a13e68fad5539a1e8e61e8ee
meinu-chan <62944053+meinu-chan@users.noreply.github.com>
2020-06-30 03:16:08 +0300

    Realize Searching By Name/Capital/Region

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+//import SortAndSearch from './SortAndSearch'
 import MainSort from './Components/Sort/MainSort';
 // import MainSearch from './Components/Search/MainSearch'
 function App() {
--- src/Components/Sort/SortPls.js
+++ src/Components/Sort/SortPls.js
@@ -17,15 +17,20 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import axios from 'axios';
 import _ from 'lodash';
 
-const API_NAME = 'https://restcountries.eu/rest/v2/name/';
-const API_ALL = 'https://restcountries.eu/rest/v2/all';
+const API = ['https://restcountries.eu/rest/v2/all','https://restcountries.eu/rest/v2/name/',['https://restcountries.eu/rest/v2/name/','?fullText=true'],'https://restcountries.eu/rest/v2/capital/','https://restcountries.eu/rest/v2/region/',]
+// const API_NAME = 'https://restcountries.eu/rest/v2/name/';
+// const API_ALL = 'https://restcountries.eu/rest/v2/all';
+// const API_FULLNAME1 = 'https://restcountries.eu/rest/v2/name/';
+// const API_FULLNAME2 = '?fullText=true'
 const DEFAULT_COUNTRIES = [];
 const value = '';
+const key = 1;
 
 export default class SortPLS extends React.Component {  
   state = {
     countries: DEFAULT_COUNTRIES,
-    value : value,
+    value,
+    key,
   };
 
   componentDidMount() {
@@ -33,7 +38,7 @@ export default class SortPLS extends React.Component {
   }
   
   loadCountries = () => {
-    axios.get(API_ALL)
+    axios.get(API[0])
     .then(response => {
       const { data: countries } = response;
       console.log('response', countries);
@@ -55,15 +60,31 @@ export default class SortPLS extends React.Component {
     this.setState({ countries });
   };
 
-  searchByName = (e) => {
+  searchByValue = (e) => {
     let text = this.state;
     text.value = e.target.value;
     this.setState(text);
+    console.log(text.value)
     //axios.get(API_NAME + text)
   }
 
-  buttonClick = () =>{
-    axios.get(API_NAME + this.state.value)
+  getKey = (e) =>{
+    let key =this.state;
+    key.key = e.target.id;
+    this.setState(key)
+    console.log(key.key);
+  }
+
+  buttonClick = (event) =>{
+    event.preventDefault();
+
+    if(this.state.value === ''){
+      this.loadCountries();
+    }
+
+    console.log(API[this.state.key] + this.state.value)
+
+    axios.get(API[this.state.key] + this.state.value)
     .then(response => {
       const { data: countries } = response;
       console.log('response', countries);
@@ -75,6 +96,10 @@ export default class SortPLS extends React.Component {
     .catch(error => {
       console.log("error", error);
     });
+  }
+
+  inserter = (list) =>{
+
   }
 
   renderTableBody() {
@@ -120,15 +145,15 @@ export default class SortPLS extends React.Component {
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                   <NavDropdown title="Search By" id="basic-nav-dropdown">
-                    <NavDropdown.Item >Name</NavDropdown.Item>
-                    <NavDropdown.Item >Full Name</NavDropdown.Item>
-                    <NavDropdown.Item >Capital</NavDropdown.Item>
-                    <NavDropdown.Item >Region</NavDropdown.Item>
+                    <NavDropdown.Item id='1' onClick={this.getKey} >Name</NavDropdown.Item>
+                    <NavDropdown.Item id='2' onClick={this.getKey} >Full Name</NavDropdown.Item>
+                    <NavDropdown.Item id='3' onClick={this.getKey} >Capital</NavDropdown.Item>
+                    <NavDropdown.Item id='4' onClick={this.getKey} >Region</NavDropdown.Item>
                   </NavDropdown>
                 </Nav>
                 <Form inline >
                   <FormControl type="text" placeholder="Search" className="mr-sm-2" ref={ref => this.input = ref}
-                    onChange={this.searchByName}
+                    onChange={this.searchByValue}
                     value={this.state.value} />
                   <Button variant="outline-success" onClick={this.buttonClick} >Search</Button>
                 </Form>
